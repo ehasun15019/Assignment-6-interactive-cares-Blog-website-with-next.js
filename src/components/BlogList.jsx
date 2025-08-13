f25fa0223ca40bec708cd70861d3122363730b0d
Your Full Name <ehasun0909@gamil.com>
2025-08-14 00:24:17 +0600

    tabs create ar css and functionality add kora hoise kinto button gola te click korle button ar color change hobe but filtering hobe na akon

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,18 +1,56 @@
 import { blog_data } from "@/assets/assets";
-import React from "react";
+import React, { useState } from "react";
 import BlogItem from "./BlogItem";
 
 const BlogList = () => {
+  const [menu, setMenu] = useState("All");
+
   return (
     <div className="px-12">
       <div className="flex justify-center gap-6 my-10">
-        <button className="bg-black text-white py-1 px-4 rounded-sm">
+        <button
+          onClick={() => setMenu("All")}
+          className={
+            menu === "All"
+              ? "bg-black text-white py-1 px-4 rounded-sm cursor-pointer"
+              : "cursor-pointer"
+          }
+        >
           All
         </button>
 
-        <button>Technology</button>
-        <button>Startup</button>
-        <button>Lifestyle</button>
+        <button
+          onClick={() => setMenu("Technology")}
+          className={
+            menu === "Technology"
+              ? "bg-black text-white py-1 px-4 rounded-sm cursor-pointer"
+              : "cursor-pointer"
+          }
+        >
+          Technology
+        </button>
+
+        <button
+          onClick={() => setMenu("Startup")}
+          className={
+            menu === "Startup"
+              ? "bg-black text-white py-1 px-4 rounded-sm cursor-pointer"
+              : "cursor-pointer"
+          }
+        >
+          Startup
+        </button>
+
+        <button
+          onClick={() => setMenu("Lifestyle")}
+          className={
+            menu === "Lifestyle"
+              ? "bg-black text-white py-1 px-4 rounded-sm cursor-pointer"
+              : "cursor-pointer"
+          }
+        >
+          Lifestyle
+        </button>
       </div>
 
       <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl-mx-24">
